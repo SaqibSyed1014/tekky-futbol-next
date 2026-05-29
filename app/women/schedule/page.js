bb73ebfea7399e9bddc165b22ccd18c804b2b2e8
murtaza1244 <murtaza@cardsiq.io>
2026-05-29 17:31:50 +0500

    schedule finale table fixes

--- app/women/schedule/page.js
+++ app/women/schedule/page.js
@@ -52,7 +52,7 @@ export default function WomenSchedulePage() {
                 <table>
                   <thead>
                   <tr>
-                    <th>Venue</th><th>Match</th><th>Time</th><th>Date</th>
+                    <th>Division</th><th>Match</th><th>Time</th><th>Date</th>
                   </tr>
                   </thead>
                   <tbody>
@@ -80,7 +80,7 @@ export default function WomenSchedulePage() {
                       <tr><th>Venue</th><th>Match</th><th>Score</th><th>Week</th><th>Date</th></tr>
                       </thead>
                       <tbody>
-                      <tr><td className="venue-cell">Tekky North</td><td></td><td></td><td></td><td></td></tr>
+                      <tr><td></td><td></td><td></td><td></td><td></td></tr>
                       </tbody>
                     </table>
                   </div>
@@ -96,7 +96,7 @@ export default function WomenSchedulePage() {
                       <tr><th>Venue</th><th>Match</th><th>Score</th><th>Week</th><th>Date</th></tr>
                       </thead>
                       <tbody>
-                      <tr><td className="venue-cell">Tekky South</td><td></td><td></td><td></td><td></td></tr>
+                      <tr><td></td><td></td><td></td><td></td><td></td></tr>
                       </tbody>
                     </table>
                   </div>
